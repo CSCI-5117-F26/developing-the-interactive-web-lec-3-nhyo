1a36e79e03d0d4ccf5b07900500088c28e9ebd23
Qiwen Pang <pqw735962446@gmail.com>
2026-09-17 18:01:11 -0500

    first page and y/n

--- HW1/server.py
+++ HW1/server.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from hw1!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/survey")
+def survey():
+    return render_template("survey.html")
+
+
+@app.route("/decline")
+def decline():
+    return render_template("decline.html")
+
+
+@app.route("/thanks")
+def thanks():
+    return render_template("thanks.html")
